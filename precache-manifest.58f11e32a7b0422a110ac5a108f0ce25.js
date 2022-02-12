855fbd99d743177e7f33f933972251c24f22ad0e
anastasia1756 <anastasiia7kolomiiets@gmail.com>
2022-02-12 13:28:36 +0200

    Updates

---
+++ precache-manifest.58f11e32a7b0422a110ac5a108f0ce25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25a43291119a0740974ae2b9fcdad9d",
+    "url": "/shopping-list-project/index.html"
+  },
+  {
+    "revision": "aa9791a2af4d748a7b93",
+    "url": "/shopping-list-project/static/css/main.e6bb01d6.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/shopping-list-project/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping-list-project/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa9791a2af4d748a7b93",
+    "url": "/shopping-list-project/static/js/main.daf2866e.chunk.js"
+  },
+  {
+    "revision": "0e4d820d47d13ec276f2",
+    "url": "/shopping-list-project/static/js/runtime-main.50d4a995.js"
+  }
+]);
